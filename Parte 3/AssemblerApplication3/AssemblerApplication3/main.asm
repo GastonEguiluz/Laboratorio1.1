b75cb2a0f89f5bfd2ba42f3485f0be68e043972d
ariel demarco <ariel.demarco@estudiantes.utec.edu.uy>
2026-08-28 23:03:19 -0300

    Agregar secuencias restantes y temporizacion

--- Parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
+++ Parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
@@ -160,20 +160,76 @@ PATRON_42_EXTERIOR:
 PATRON_81_EXTERIOR:
     ldi patron, 0x81
     ret
+
 SECUENCIA_5:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_5
+    ldi patron, 0xAA
+
+MOSTRAR_SECUENCIA_5:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    com patron
     ret
 
 SECUENCIA_6:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_6
+    ldi patron, 0x01
+
+MOSTRAR_SECUENCIA_6:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    cpi patron, 0xFF
+    breq REINICIAR_SECUENCIA_6
+    lsl patron
+    ori patron, 0x01
+    ret
+
+REINICIAR_SECUENCIA_6:
+    clr patron
     ret
 
 SECUENCIA_7:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_7
+    ldi patron, 0x03
+
+MOSTRAR_SECUENCIA_7:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    cpi patron, 0xC0
+    breq REINICIAR_SECUENCIA_7
+    lsl patron
+    lsl patron
+    ret
+
+REINICIAR_SECUENCIA_7:
+    clr patron
     ret
 
 SECUENCIA_8:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    com patron
     ret
 
 RETARDO_PASO:
-    ret
+    ldi retardo1, 16
 
+RETARDO_PASO_1:
+    ldi retardo2, 255
+
+RETARDO_PASO_2:
+    ldi retardo3, 255
+
+RETARDO_PASO_3:
+    dec retardo3
+    brne RETARDO_PASO_3
+    dec retardo2
+    brne RETARDO_PASO_2
+    dec retardo1
+    brne RETARDO_PASO_1
+    ret
 LEER_BOTONES:
     ret
