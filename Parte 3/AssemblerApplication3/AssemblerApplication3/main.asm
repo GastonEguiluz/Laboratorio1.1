edad58de4477c2afe88edd19d193185a9673d3a9
ariel demarco <ariel.demarco@estudiantes.utec.edu.uy>
2026-08-28 23:02:31 -0300

    Implementar seleccion y primeras cuatro secuencias LED

--- Parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
+++ Parte 3/AssemblerApplication3/AssemblerApplication3/main.asm
@@ -34,7 +34,142 @@ PRINCIPAL:
     rcall LEER_BOTONES
     rcall EJECUTAR_SECUENCIA
     rjmp PRINCIPAL
+
 EJECUTAR_SECUENCIA:
+    cpi secuencia, 0
+    breq LLAMAR_SECUENCIA_1
+    cpi secuencia, 1
+    breq LLAMAR_SECUENCIA_2
+    cpi secuencia, 2
+    breq LLAMAR_SECUENCIA_3
+    cpi secuencia, 3
+    breq LLAMAR_SECUENCIA_4
+    cpi secuencia, 4
+    breq LLAMAR_SECUENCIA_5
+    cpi secuencia, 5
+    breq LLAMAR_SECUENCIA_6
+    cpi secuencia, 6
+    breq LLAMAR_SECUENCIA_7
+    rcall SECUENCIA_8
+    ret
+
+LLAMAR_SECUENCIA_1:
+    rcall SECUENCIA_1
+    ret
+
+LLAMAR_SECUENCIA_2:
+    rcall SECUENCIA_2
+    ret
+
+LLAMAR_SECUENCIA_3:
+    rcall SECUENCIA_3
+    ret
+
+LLAMAR_SECUENCIA_4:
+    rcall SECUENCIA_4
+    ret
+
+LLAMAR_SECUENCIA_5:
+    rcall SECUENCIA_5
+    ret
+
+LLAMAR_SECUENCIA_6:
+    rcall SECUENCIA_6
+    ret
+
+LLAMAR_SECUENCIA_7:
+    rcall SECUENCIA_7
+    ret
+
+SECUENCIA_1:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_1
+    ldi patron, 0x01
+
+MOSTRAR_SECUENCIA_1:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    lsl patron
+    ret
+
+SECUENCIA_2:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_2
+    ldi patron, 0x80
+
+MOSTRAR_SECUENCIA_2:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    lsr patron
+    ret
+
+SECUENCIA_3:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_3
+    ldi patron, 0x81
+
+MOSTRAR_SECUENCIA_3:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    cpi patron, 0x81
+    breq PATRON_42
+    cpi patron, 0x42
+    breq PATRON_24
+    cpi patron, 0x24
+    breq PATRON_18
+    clr patron
+    ret
+
+PATRON_42:
+    ldi patron, 0x42
+    ret
+
+PATRON_24:
+    ldi patron, 0x24
+    ret
+
+PATRON_18:
+    ldi patron, 0x18
+    ret
+
+SECUENCIA_4:
+    cpi patron, 0
+    brne MOSTRAR_SECUENCIA_4
+    ldi patron, 0x18
+
+MOSTRAR_SECUENCIA_4:
+    out PORTD, patron
+    rcall RETARDO_PASO
+    cpi patron, 0x18
+    breq PATRON_24_EXTERIOR
+    cpi patron, 0x24
+    breq PATRON_42_EXTERIOR
+    cpi patron, 0x42
+    breq PATRON_81_EXTERIOR
+    clr patron
+    ret
+
+PATRON_24_EXTERIOR:
+    ldi patron, 0x24
+    ret
+
+PATRON_42_EXTERIOR:
+    ldi patron, 0x42
+    ret
+
+PATRON_81_EXTERIOR:
+    ldi patron, 0x81
+    ret
+SECUENCIA_5:
+    ret
+
+SECUENCIA_6:
+    ret
+
+SECUENCIA_7:
+    ret
+
+SECUENCIA_8:
     ret
 
 RETARDO_PASO:
